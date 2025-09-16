f0896c51e8c55c1a1b5b9f615e3f8237b02b2dbd
HenryChang <henry930619@gmail.com>
2025-09-16 21:37:41 +0800

    unfished feat: websocket connection

--- routes/message.py
+++ routes/message.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from websocket.connec_manager import ConnectionManager
+
+manager = ConnectionManager()
 
 router = APIRouter(
     prefix="/messages",
@@ -17,5 +20,14 @@ async def get_message(message_id: int):
 
 
 @router.websocket("/ws/{message_id}")
-async def websocket_endpoint(message_id: int):
-    pass
+async def websocket_endpoint(websocket: WebSocket, message_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
