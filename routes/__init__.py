f1232447dfefae3185a5092cfd390fab908e2ab9
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2025-09-20 01:17:41 +0800

    include category route

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from .category import router as category_router
 from .user import router as user_router
 
 ROUTER = APIRouter()
 
+ROUTER.include_router(category_router)
 ROUTER.include_router(user_router)
